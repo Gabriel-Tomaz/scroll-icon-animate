1072ca470dee79c281bfa51b01272d8b847a8f03
Gabriel-Tomaz <jgabrieltomaz2706@gmail.com>
2020-06-22 10:08:55 -0300

    Otimizando o código.

--- js/main.js
+++ js/main.js
@@ -5,9 +5,12 @@ function scrollAnimate(){
    //Capturando a distancia entre a barra de scroll e o topo da página; 
    const windowTop = window.pageYOffset;
    target.classList.add(animateClass);
+
+   if(windowTop === 0){
+    target.classList.remove(animateClass);
+   }
 }
 
 window.addEventListener('scroll', () => {
-    target.classList.remove(animateClass);
     scrollAnimate();
 });
