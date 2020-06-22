24da4336bfdd87e2c81bc67be4791c68c75ead19
Gabriel-Tomaz <jgabrieltomaz2706@gmail.com>
2020-06-22 09:39:15 -0300

    removendo função inutilizada.

--- js/main.js
+++ js/main.js
@@ -2,8 +2,6 @@ const target = document.querySelector('.scroller');
 const animateClass = 'scroller-animate';
 
 function scrollAnimate(){
-   //Capturando a distancia entre a barra de scroll e o topo da página; 
-   const windowTop = window.pageYOffset;
    target.classList.add(animateClass);
 }
 
