3af8f4f0e2a711f5776386f7fc5fb702261b1cc1
Gabriel-Tomaz <jgabrieltomaz2706@gmail.com>
2020-06-22 09:49:13 -0300

    A função estava sendo utilizada.

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ const target = document.querySelector('.scroller');
 const animateClass = 'scroller-animate';
 
 function scrollAnimate(){
+   //Capturando a distancia entre a barra de scroll e o topo da página; 
+   const windowTop = window.pageYOffset;
    target.classList.add(animateClass);
 }
 
